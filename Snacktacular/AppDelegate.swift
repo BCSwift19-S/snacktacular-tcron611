f293a75d7ae79319883e1c800d0e98a05023381f
Thomas Ronan <ronantho@bc.edu>
2019-04-01 05:06:19 -0400

    9.4 Complete

--- Snacktacular/AppDelegate.swift
+++ Snacktacular/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-     
+        GMSPlacesClient.provideAPIKey("AIzaSyDHNJf2QbTv8KD7oa4StnzbOaQFCbpTza0")
         FirebaseApp.configure()
         return true
     }
