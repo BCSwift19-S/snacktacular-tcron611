712100e57e49e09ade07055b287e652d9415c928
Thomas Ronan <ronantho@bc.edu>
2019-04-15 13:07:36 -0400

    9.13 Complete

---
+++ Snacktacular/ReviewTableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ReviewTableViewController.swift
+//  Snacktacular
+//
+//  Created by Thomas Ronan on 4/15/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewController: UITableViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var postedByLabel: UILabel!
+    @IBOutlet weak var reviewTitleField: UITextField!
+    @IBOutlet weak var reviewDateLabel: UILabel!
+    @IBOutlet weak var reviewTextView: UITextView!
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var buttonsBackgroundView: UIView!
+    @IBOutlet var starButtonCollection: [UIButton]!
+    
+    var rating = 0 {
+        didSet {
+            for starButton in starButtonCollection {
+                let image = UIImage(named: (starButton.tag < rating ? "star-filled": "star-empty"))
+               starButton.setImage(image, for: .normal)
+            }
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+        
+    }
+    func leaveViewController() {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    @IBAction func starButtonPressed(_ sender: UIButton) {
+        rating = sender.tag + 1
+    }
+    
+    
+    @IBAction func reviewTitleChanged(_ sender: UITextField) {
+    }
+    
+    @IBAction func returnTitleDonePressed(_ sender: UITextField) {
+    }
+    
+    @IBAction func deleteButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        leaveViewController()
+    }
+    
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    
+
+
+}
